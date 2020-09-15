ef72a79a45b0a32cb825962ab8d804a7c2a3f5cf
Osvaldo Costa <osvaldo.costajr@gmail.com>
2020-09-15 13:39:32 -0300

    DOM JS done

---
+++ JS/beluga.js
@@ -0,0 +1,8 @@
+function calcularMedia(){
+    var media = parseFloat(0);
+    var i = 0;
+    for(i = 0; i < arguments.length; i++){
+     media += arguments[i];
+    }
+    return media = media/i;
+ }
